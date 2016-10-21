0d948f2571bbc8a4cdd75d6c324377325ec00616
nickMontoya <nimo0295@colorado.edu>
2016-10-21 17:57:01 -0600

    Added styling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app   = express();
 var path  = require("path");
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/index.html'));
   //__dirname : It will resolve to your project folder.
@@ -17,3 +19,5 @@ app.get('/login',function(req,res){
 
 app.listen(5000);
 console.log("Server running on localhost:5000...");
+console.log("The server will automatically refresh when new code is added");
+console.log("Press ctrl-c to stop the server");
