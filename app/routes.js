d71c87b90a8be5ece9aeefb6689c643a984a1c91
Daniel Schwabacher <danielschwabacher@gmail.com>
2016-11-24 01:00:03 -0500

    got input forms to display in console on sumbit in addContact.

--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,51 @@
 // app/routes.js
+var express = require('express')
+var app = express()
 module.exports = function(app, passport) {
-
-	// =====================================
 	// HOME PAGE (with login links) ========
-	// =====================================
 	app.get('/', function(req, res) {
 		res.render('index.ejs'); // load the index.ejs file
 	});
 
-	// =====================================
 	// LOGIN ===============================
-	// =====================================
-	// show the login form
 	app.get('/login', function(req, res) {
-
 		// render the page and pass in any flash data if it exists
 		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+
+	// SIGNUP ==============================
+	app.get('/signup', function(req, res) {
+		// render the page and pass in any flash data if it exists
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+	// Add New Contact =====================
+	app.get('/addcontact', function(req, res) {
+		// render the page and pass in any flash data if it exists
+		res.render('addcontact.ejs', {
+            message: req.flash('Add Contact Message')
+        });
+	});
+
+	// Donate Route +++=====================
+	app.get('/donate', function(req, res) {
+		// render the page and pass in any flash data if it exists
+		res.render('donate.ejs', { message: req.flash('Donate Message') });
+	});
+
+	// PROFILE SECTION =========================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	// LOGOUT ==============================
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
 	});
 
 	// process the login form
@@ -25,15 +55,6 @@ module.exports = function(app, passport) {
 		failureFlash : true // allow flash messages
 	}));
 
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	// show the signup form
-	app.get('/signup', function(req, res) {
-
-		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
 
 	// process the signup form
 	app.post('/signup', passport.authenticate('local-signup', {
@@ -41,48 +62,10 @@ module.exports = function(app, passport) {
 		failureRedirect : '/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
-
-	// =====================================
-	// PROFILE SECTION =========================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
-
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
-
-
-	// =====================================
-	// Add New Contact =====================
-	// =====================================
-	app.get('/addcontact', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('addcontact.ejs', { message: req.flash('Add Contact Message') });
-	});
-
-
-	// =====================================
-	// Donate Route +++=====================
-	// =====================================
-	app.get('/donate', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('donate.ejs', { message: req.flash('Donate Message') });
-	});
 };
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
-
 	// if user is authenticated in the session, carry on
 	if (req.isAuthenticated())
 		return next();
